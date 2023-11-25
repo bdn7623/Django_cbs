b37986d3bc9cc1b20fc054bf22553af731eed2b5
bdn7623 <bdn7623.py@gmail.com>
2023-11-25 15:51:55 +0200

    Homework 4

--- main/models.py
+++ main/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.db.models import CASCADE
 
-
 # Create your models here.
+
 class Category(models.Model):
+    """
+    Represents a product category.
+
+    Attributes:
+    - category_name (str): The name of the category.
+    """
+
     category_name = models.CharField(max_length=150, null=True, blank=True)
 
     def __str__(self):
@@ -11,6 +18,19 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """
+    Represents a product.
+
+    Attributes:
+    - product_name (str): The name of the product.
+    - price (int): The price of the product.
+    - description (str): A brief description of the product.
+    - discount (int): The discount percentage applied to the product.
+    - category (Category): The category to which the product belongs.
+    - country (str): The country of origin for the product.
+    - firm (str): The manufacturing firm of the product.
+    """
+
     product_name = models.CharField(max_length=255, null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
@@ -24,6 +44,14 @@ class Product(models.Model):
 
 
 class Bucket(models.Model):
+    """
+    Represents a user's shopping bucket.
+
+    Attributes:
+    - owner_name (str): The name of the bucket owner.
+    - products (ManyToManyField): The products added to the bucket.
+    """
+
     owner_name = models.CharField(max_length=255, null=True, blank=True)
     products = models.ManyToManyField(Product)
 
@@ -32,6 +60,16 @@ class Bucket(models.Model):
 
 
 class Client(models.Model):
+    """
+    Represents a client/user.
+
+    Attributes:
+    - user_name (str): The username of the client.
+    - name (str): The name of the client.
+    - email (str): The email address of the client.
+    - bucket (Bucket): The shopping bucket associated with the client.
+    """
+
     user_name = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(default='qwert@gmai.com', null=True)
@@ -42,6 +80,15 @@ class Client(models.Model):
 
 
 class City(models.Model):
+    """
+    Represents a city.
+
+    Attributes:
+    - city_name (str): The name of the city.
+    - country (str): The country in which the city is located.
+    - client (ManyToManyField): The clients associated with the city.
+    """
+
     city_name = models.CharField(max_length=255, null=True, blank=True)
     country = models.CharField(max_length=255, null=True, blank=True)
     client = models.ManyToManyField(Client)
--- main/views.py
+++ main/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from .models import *
+from .models import Category
 
 # Create your views here.
 def categories(request):
+    """
+    View function to display a list of product categories.
+
+    Retrieves all categories from the database and renders them in the 'main/categories.html' template.
+
+    Args:
+    - request (HttpRequest): The HTTP request object.
+
+    Returns:
+    - HttpResponse: Rendered HTML response containing the list of categories.
+    """
+
     categories = Category.objects.all()
 
     context = {
         'categories': categories
     }
-
 
     return render(request, 'main/categories.html', context)
